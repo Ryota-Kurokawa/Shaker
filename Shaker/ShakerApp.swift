dc2bff0f102be45da83cc50785fcd969f07fbb71
ryota <kurokawa.ryota.1124@gmail.com>
2024-02-22 17:59:52 +0900

    initialization

---
+++ Shaker/ShakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakerApp.swift
+//  Shaker
+//
+//  Created by 黒川良太 on 2024/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ShakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShakerUITests/ShakerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShakerUITestsLaunchTests.swift
+//  ShakerUITests
+//
+//  Created by 黒川良太 on 2024/02/22.
+//
+
+import XCTest
+
+final class ShakerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
